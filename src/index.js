e269ac546664802ee787aa18817e466acd14bba4
veronmolosh <nikabu17@gmail.com>
2021-03-14 16:21:52 +0300

    feat: add solution task brackets 17/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [],
+        open = ['{', '(', '['],
+        close = ['}', ')', ']'],
+        elements = str.split(''),
+        openIndex,
+        closeIndex;
+    // Проходимся по строке, проверяя каждый ее символ
+    for (let i = 0; i < elements.length; i++) {
+        openIndex = open.indexOf(elements[i]);
+        if (openIndex !== -1) {
+            // Нашли открывающую скобку. Помещаем ее в стек
+            stack.push(openIndex);
+            continue;
+        }
+        closeIndex = close.indexOf(elements[i]);
+        if (closeIndex !== -1) {
+            // Нашли закрывающую скобку. Проверяем ее соответствие открывающей
+            openIndex = stack.pop();
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+        }
+    }
+    // Проверяем дисбаланс открытых/закрытых скобок
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
 }
